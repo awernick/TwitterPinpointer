5939f4d81c9a0cb464d74ad3a3dc7c3133ad5621
Alan <alan.wernik@gmail.com>
2016-10-01 16:05:44 -0500

    Setup twitter and SocketIO

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, abort, request, jsonify
-app = Flask(__name__)
+from os             import environ
+from twitter        import *
+from flask          import Flask, abort, request, jsonify
+from flask_socketio import SocketIO, send, emit
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY') or \
+                           'supercalifragilisticexpiralidocious'
+
+# SocketIO
+socketio = SocketIO(app)
+
+# Twitter
+consumer_key    = environ['TWITTER_CONSUMER_KEY']
+consumer_secret = environ['TWITTER_CONSUMER_SECRET']
+auth_token      = environ['TWITTER_AUTH_KEY']
+auth_secret     = environ['TWITTER_AUTH_SECRET']
+
+twitter = Twitter(auth=OAuth(auth_token, auth_secret,
+                             consumer_key, consumer_secret))
+
+# RESTful
 @app.route('/tweets', methods=['GET', 'POST'])
 def get_tweets():
     pass
@@ -9,7 +28,11 @@ def get_tweets():
 def get_tweet(tweet_id):
     pass
 
+@app.route('/tweets/live', methods=['POST'])
+def get_tweet_stream():
+    pass
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
 
