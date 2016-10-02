0d76f26b01c6dc7f0cda52848bf3b87d69cf8bf9
Alan <alan.wernik@gmail.com>
2016-10-01 19:08:49 -0500

    Initial implementation for Twitter Streamin API

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import json
+import thread
+import eventlet
+eventlet.monkey_patch()
+
 from os             import environ
 from twitter        import *
-from flask          import Flask, abort, request, jsonify
+from flask          import Flask, abort, request, jsonify, \
+                           copy_current_request_context
 from flask_socketio import SocketIO, send, emit
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY') or \
@@ -16,8 +23,12 @@ consumer_secret = environ['TWITTER_CONSUMER_SECRET']
 auth_token      = environ['TWITTER_AUTH_KEY']
 auth_secret     = environ['TWITTER_AUTH_SECRET']
 
-twitter = Twitter(auth=OAuth(auth_token, auth_secret,
-                             consumer_key, consumer_secret))
+auth           = OAuth(auth_token, auth_secret, consumer_key, consumer_secret)
+twitter        = Twitter(auth=auth)
+twitter_stream = TwitterStream(auth=auth)
+
+# hilary_stream  = twitter_stream.statuses.filter(track='Hilary Clinton')
+#trump_stream   = twitter_stream.statuses.filter(track='Donald Trump')
 
 # RESTful
 @app.route('/tweets', methods=['GET', 'POST'])
@@ -28,9 +39,19 @@ def get_tweets():
 def get_tweet(tweet_id):
     pass
 
-@app.route('/tweets/live', methods=['POST'])
-def get_tweet_stream():
-    pass
+# WebSocket
+@socketio.on('connect')
+def client_connected():
+    print "Client connected"
+
+    # Start stream processing
+    @copy_current_request_context
+    def process_stream(event, strm):
+        stream = strm.statuses.filter(track='Hilary Clinton')
+        for tweet in stream:
+            emit(event, tweet)
+
+    eventlet.spawn(process_stream, 'clinton_tweet', twitter_stream)
 
 
 if __name__ == '__main__':
